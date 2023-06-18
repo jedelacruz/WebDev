9657fc437ffaa4fd7708c9f777627c1655d65bee
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-06-18 22:35:43 +0800

    Add files via upload

---
+++ welcome-back-page/app.js
@@ -0,0 +1,8 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Je Dela Cruz"
+let greeting = "Welcome back"
+
+let fullMessage = greeting + " " + name
+
+welcomeEl.innerText = fullMessage
