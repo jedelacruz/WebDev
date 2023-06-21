248df34402b7739239efd455f8967a1e19b146c0
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-06-21 22:30:53 +0800

    Add files via upload

---
+++ arrays/app.js
@@ -0,0 +1,12 @@
+// string arrays
+
+let myResume = [
+    "Je Dela Cruz",
+    "16 years old",
+    "I've been coding since I was 14",
+    "I am a consistent w honors student that has 90+ average",
+    "I am a hardworking and kind person"
+]
+
+console.log(myResume[1])
+
