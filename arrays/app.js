4ccf8280b1d8acb62edb790321610bde7ac270ca
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-06-22 17:06:15 +0800

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -1,5 +1,7 @@
 // string arrays
 
+let myResumeVar = document.querySelector("#resume-el")
+
 let myResume = [
     "Je Dela Cruz",
     "16 years old",
@@ -8,5 +10,42 @@ let myResume = [
     "I am a hardworking and kind person"
 ]
 
-console.log(myResume[1])
+myResumeVar.textContent = myResume[0]
+
+// complex arrays
+
+let meVar = document.querySelector("#me-el")
+
+let me = [
+    "Je Dela Cruz",
+    16,
+    likesPizza = true
+]
+
+meVar.textContent = me[1]
+
+// add and removing items from arrays
+
+let cardsVar = document.querySelector("#cardPush-el")
+let cards = [7, 4]
+
+cards.push(6)
+
+cardsVar.textContent = cards
+
+// adding & delete  arrays ex#1
+
+let messagesVar = document.querySelector("#messagesPush-el")
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.pop(messages)
+messages.push(newMessage)
+
+messagesVar.innerHTML = messages
 
