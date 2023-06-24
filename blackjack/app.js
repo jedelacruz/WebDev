afb4884238b673ad13f50ada9cd8fca2104fa967
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-06-24 22:31:25 +0800

    Update app.js

--- blackjack/app.js
+++ blackjack/app.js
@@ -1,20 +1,45 @@
-let firstCard = 1
-let secondCard = 11
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+console.log(cards)
+
+function getRandomCard(){
+    let randomNumber = Math.floor( Math.random() * 13) + 1
+    
+    if(randomNumber > 10){
+        return 10
+    }
+    else if(randomNumber === 1){
+        return 11
+    }
+    else{
+        return randomNumber
+    }
+}
 
 function startGame(){
-    renderGame()
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()  
 }
 
 function renderGame(){
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    //cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -33,8 +58,14 @@ function renderGame(){
 }
 
 function newCard(){
-    console.log("drawing a new card")
-    let card = 2
-    sum += card
-    renderGame()
+
+    if(sum === 0 || sum >= 21){
+
+    }
+    else{
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
